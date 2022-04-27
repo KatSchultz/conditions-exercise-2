dc181a831d26e4b587ca4ec14c5edf9b2a85941b
Katie Schultz <ks.katieschultz@gmail.com>
2022-04-27 13:31:58 -0400

    nested some for loops and added css for fun

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const name1 = "Sherry Champaign1";
-const name2 = "Sherry Champaign2";
-const name3 = "Sherry Champaign3";
+const name1 = "Sherry Champaign";
+const name2 = "Tom";
+const name3 = "Charlie";
 
 const n1 = name1.length;
 const n2 = name2.length;
@@ -18,24 +18,14 @@ if (n3 > n1 && n3 > n2) {
   console.log(`${name3} has the longest name.`);
 }
 
-if (n1 === n2 && n1 > n3) {
-  console.log(`${name1} and ${name2} tie for the longest name.`);
-}
-
-if (n1 === n3 && n1 > n2) {
-  console.log(`${name1} and ${name3} tie for the longest name.`);
-}
-
-if (n2 === n3 && n2 > n1) {
-  console.log(`${name2} and ${name3} tie for the longest name.`);
-}
-
 if (n1 === n2 && n2 === n3) {
   console.log(
     `All three names ${name1}, ${name2}, and ${name3} are the same length.`
   );
+} else if (n1 === n2 && n1 > n3) {
+  console.log(`${name1} and ${name2} tie for the longest name.`);
+} else if (n1 === n3 && n1 > n2) {
+  console.log(`${name1} and ${name3} tie for the longest name.`);
+} else if (n2 === n3 && n2 > n1) {
+  console.log(`${name2} and ${name3} tie for the longest name.`);
 }
-
-console.log("name 1" + name1.length);
-console.log("name 2" + name2.length);
-console.log("name 3" + name3.length);
